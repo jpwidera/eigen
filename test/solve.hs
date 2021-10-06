ba158568f77667f438ca1821f70c8c65dee6e9fd
jwidera <widera@helzel.com>
2021-10-06 10:35:06 +0200

    Maybe fixed struct

--- test/solve.hs
+++ test/solve.hs
@@ -2,14 +2,13 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
 import Eigen.Matrix
-import Eigen.Solver.LA
-import Data.Maybe (fromMaybe)
 
-solution :: Maybe (MatrixXd 3 1)
+solution :: IO ()
 solution = do
-  _a :: MatrixXd 3 3 <- fromList [[1,2,3], [4,5,6], [7,8,9]]
-  _b :: MatrixXd 3 1 <- fromList [[3],[3],[4]]
-  pure $ solve ColPivHouseholderQR _a _b
+  let a = empty :: Matrix Float
+  print a
+  return  ()
+
 
 main :: IO ()
 main = print solution
